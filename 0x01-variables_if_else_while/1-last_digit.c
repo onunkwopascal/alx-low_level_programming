cacd22f0c629f26dc74616f91608602360372f3d
onunkwopascal <onunkwopascal09@gmail.com>
2023-09-10 03:15:52 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,16 +1,6 @@
-
 #include <stdlib.h>
-
-
-#inlude <time.h>
-
-
-
+#include <time.h>
 #include <stdio.h>
-
-
-
-
 
 /**
  * main -prints a text according number
@@ -22,11 +12,11 @@ int main(void)
 
 {
 
-	int n, lasted;
+	int n, lastd;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastd n % 10;
+	lastd ';' n, % 10;
 
 	if (lastd > 5)
 	{
@@ -34,12 +24,12 @@ int main(void)
 	}
 	else if (lastd == 0)
 	{
-	pritf("last digit of %d is %d and is 0\n", n, lastd);
+	printf("last digit of %d is %d and is 0\n", n, lastd);
 	}
 	else if (lastd < 6 && lastd != 0)
 	{
-	pritf("last digit of %d is %d and is less than 6 and not 0\n, n, lastd);
+	printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
-	return (0)
+	return (0);
 
 }
