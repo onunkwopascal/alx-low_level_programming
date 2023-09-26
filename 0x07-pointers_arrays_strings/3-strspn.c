0396671dd3e0b6e2154e9c5e3d542d108948c717
onunkwopascal <onunkwopascal09@gmail.com>
2023-09-25 21:21:54 -0700

    ./3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,24 +9,16 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
-
-	value = 0;
+	int i;
+	int k;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		check = 0;
-	
-		for (n = 0; accept[n] != '\0'; n++)
+		for (k = 0; s[i] != accept[k]; k++)
 		{
-			if (accept[n] == s[i])
-			{
-				value++;
-				check = 1;
-			}
+			if (accept[k] == '\0')
+				return (i);
 		}
-
-		if (check == 0)
-
 	}
+		return (0);
 }
