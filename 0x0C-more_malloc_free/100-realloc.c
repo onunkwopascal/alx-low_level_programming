671127f52702b346cf8db48c358a576bb7c201c9
onunkwopascal <onunkwopascal09@gmail.com>
2023-10-08 21:10:52 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -44,7 +44,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	for (i = 0; i < old_size && i < new_size; i++)
 	{
-		nptr[i] = ((char *) ptr) [i];
+		nptr[i] = ((char *) ptr)[i];
 	}
 
 	free(ptr);
