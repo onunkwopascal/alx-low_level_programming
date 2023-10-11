9c94a1c87b0e50e3c93a2d1d913ef762cae63710
onunkwopascal <onunkwopascal09@gmail.com>
2023-10-10 19:40:33 -0700

    ./a

--- 0x0E-structures_typedef/4-main.c
+++ 0x0E-structures_typedef/4-main.c
@@ -10,6 +10,7 @@ int main(void)
 {
 
 	my_dog = new_dog("Poppy", 3.5, "Bob");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age)
+		;
 	return (0);
 }
